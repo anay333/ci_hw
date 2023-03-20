7140fc8e7046f3fc7de1c5859929b535cf0788d0
anay333 <87089288+anay333@users.noreply.github.com>
2023-03-21 02:01:57 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "4a9hn2",
+  projectId: "zcapx8",
   e2e: {
     baseUrl: 'http://qamid.tmweb.ru/',
     setupNodeEvents(on, config) {
